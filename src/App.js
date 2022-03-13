4721f6a6b6e5497900df7e1344619079c3a516c0
MiloShen <Milo-Shen@outlook.com>
2022-03-13 17:07:33 +0800

    feat: update demo

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './App.css';
 
 function App() {
   const [jsTime, setJsTime] = useState({ time: '0', result: '0' });
-  const [wasmTime, setWasmTime] = useState('0');
+  const [wasmTime, setWasmTime] = useState({ time: '0', result: '0' });
 
   useEffect(() => {
     (async () => {
@@ -26,8 +26,11 @@ function App() {
 
       // WebAssembly
       start = performance.now();
-      run_fibonacci(30, 30);
-      setWasmTime((performance.now() - start).toFixed(2));
+      result = run_fibonacci(30, 30);
+      setWasmTime({
+        time: (performance.now() - start).toFixed(2),
+        result
+      });
     })();
   }, []);
 
@@ -35,8 +38,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>WebAssembly Elapsed Time: {wasmTime} ms</p>
+        <p>WebAssembly Elapsed Time: {wasmTime.time} ms</p>
+        <p className="result">WebAssembly result: {wasmTime.result}</p>
         <p>Javascript Elapsed Time: {jsTime.time} ms</p>
+        <p className="result">Javascript result: {jsTime.result}</p>
       </header>
     </div>
   );
