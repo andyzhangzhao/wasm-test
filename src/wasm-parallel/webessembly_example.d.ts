fc2a0b22e67724743480c5aa594879b6cef31b4e
MiloShen <Milo-Shen@outlook.com>
2022-03-17 20:04:29 +0800

    feat: add wasm parallel

---
+++ src/wasm-parallel/webessembly_example.d.ts
@@ -0,0 +1,80 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export function greet(): void;
+/**
+* @param {number} iter
+* @param {number} len
+* @returns {number}
+*/
+export function run_fibonacci(iter: number, len: number): number;
+/**
+* @param {number} num
+* @returns {number}
+*/
+export function plus_ten(num: number): number;
+/**
+* @param {number} num
+* @returns {number}
+*/
+export function plus_ten_simd(num: number): number;
+/**
+* @param {number} num
+* @returns {number}
+*/
+export function plus_ten_simd_threads(num: number): number;
+/**
+* @param {number} num_threads
+* @returns {Promise<any>}
+*/
+export function initThreadPool(num_threads: number): Promise<any>;
+/**
+* @param {number} receiver
+*/
+export function wbg_rayon_start_worker(receiver: number): void;
+/**
+*/
+export class wbg_rayon_PoolBuilder {
+  free(): void;
+/**
+* @returns {number}
+*/
+  numThreads(): number;
+/**
+* @returns {number}
+*/
+  receiver(): number;
+/**
+*/
+  build(): void;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly greet: () => void;
+  readonly run_fibonacci: (a: number, b: number) => number;
+  readonly plus_ten: (a: number) => number;
+  readonly plus_ten_simd: (a: number) => number;
+  readonly plus_ten_simd_threads: (a: number) => number;
+  readonly __wbg_wbg_rayon_poolbuilder_free: (a: number) => void;
+  readonly wbg_rayon_poolbuilder_numThreads: (a: number) => number;
+  readonly wbg_rayon_poolbuilder_receiver: (a: number) => number;
+  readonly wbg_rayon_poolbuilder_build: (a: number) => void;
+  readonly initThreadPool: (a: number) => number;
+  readonly wbg_rayon_start_worker: (a: number) => void;
+  readonly memory: WebAssembly.Memory;
+  readonly __wbindgen_start: () => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+* @param {WebAssembly.Memory} maybe_memory
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>, maybe_memory?: WebAssembly.Memory): Promise<InitOutput>;
---
+++ src/wasm-parallel/webessembly_example_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export function greet(): void;
+export function run_fibonacci(a: number, b: number): number;
+export function plus_ten(a: number): number;
+export function plus_ten_simd(a: number): number;
+export function plus_ten_simd_threads(a: number): number;
+export function __wbg_wbg_rayon_poolbuilder_free(a: number): void;
+export function wbg_rayon_poolbuilder_numThreads(a: number): number;
+export function wbg_rayon_poolbuilder_receiver(a: number): number;
+export function wbg_rayon_poolbuilder_build(a: number): void;
+export function initThreadPool(a: number): number;
+export function wbg_rayon_start_worker(a: number): void;
+export const memory: WebAssembly.Memory;
+export function __wbindgen_start(): void;
